b624b48721fba87a731113c2287d7113fa9c0e7e
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-12-25 00:02:37 +0530

    first

---
+++ Slip_18/Q21.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Q21
+{
+    pubic static void main(String[] args)
+    {
+        FileInputStream fis = new FileInputStream("Ball.txt")
+    }
+}
